f22a45c7fb20d09cafa6868fa0609d963084e800
Bayr-Ka <kachkanov.b@gmail.com>
2025-02-24 20:39:39 +0000

    Update knight.py

--- classes/pieces/knight.py
+++ classes/pieces/knight.py
@@ -2,15 +2,17 @@ from classes.pieces.piece import Piece
 
 
 class Knight(Piece):
-    def __init__(self, colour, position, cell_size):
+    def __init__(self, color, position, cell_size, start_x, start_y):
         """
         Initialize the knight.
         :param colour: Knight colour ("black" or "white").
         :param position: Knight position on the board as a tuple (row, col).
         :param cell_size: Board cell size.
+        :param start_x: Initial X coordinates.
+        :param start_y: Initial Y coordinates.
         """
         image_name = "bN.png" if colour == "black" else "wN.png"
-        super().__init__(colour, position, cell_size, image_name)
+        super().__init__(color, position, cell_size, image_name, start_x, start_y)
 
     def get_valid_moves(self, board):
         """
